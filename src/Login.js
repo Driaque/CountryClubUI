7d6d361b1025bf11622478d5aff0656ccb92a27a
Derrq <uka.onyeani@gmail.com>
2019-07-12 20:47:05 -0400

    React Boostrap integration

--- src/Login.js
+++ src/Login.js
@@ -1,45 +1,39 @@
 import React, { Component } from 'react'
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import "./Login.css";
 
 export default class Login extends Component {
     render() {
         return (
-            <div className="login-box-body">
-                <p className="login-box-msg">Sign in to start your session</p>
-                <form action="../../index2.html" method="post">
-                    <div className="form-group has-feedback">
-                        <input type="email" className="form-control" placeholder="Email" />
-                        <span className="glyphicon glyphicon-envelope form-control-feedback" />
-                    </div>
-                    <div className="form-group has-feedback">
-                        <input type="password" className="form-control" placeholder="Password" />
-                        <span className="glyphicon glyphicon-lock form-control-feedback" />
-                    </div>
-                    <div className="row">
-                        <div className="col-xs-8">
-                            <div className="checkbox icheck">
-                                <label>
-                                    <input type="checkbox" /> Remember Me
-                                </label>
-                            </div>
-                        </div>
-                        {/* /.col */}
-                        <div className="col-xs-4">
-                            <button type="submit" className="btn btn-primary btn-block btn-flat">Sign In</button>
-                        </div>
-                        {/* /.col */}
-                    </div>
-                </form>
-                <div className="social-auth-links text-center">
-                    <p>- OR -</p>
-                    <a href="#" className="btn btn-block btn-social btn-facebook btn-flat"><i className="fa fa-facebook" /> Sign in using
-                 Facebook</a>
-                    <a href="#" className="btn btn-block btn-social btn-google btn-flat"><i className="fa fa-google-plus" /> Sign in using
-                 Google+</a>
-                </div>
-                {/* /.social-auth-links */}
-                <a href="#">I forgot my password</a><br />
-                <a href="register.html" className="text-center">Register a new membership</a>
-            </div>
+            <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="email" bsSize="large">
+            <ControlLabel>Email</ControlLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+            />
+          </FormGroup>
+          <Button
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+          >
+            Login
+          </Button>
+        </form>
+      </div>
 
         )
     }
