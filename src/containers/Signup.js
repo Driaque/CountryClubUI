62dc51dd7ab3e9935d880d18384325c10d06b6a5
Derrq <uka.onyeani@gmail.com>
2019-07-19 15:57:58 -0400

    Sign Up UI2 sign up working

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -3,10 +3,19 @@ import {
   HelpBlock,
   FormGroup,
   FormControl,
-  ControlLabel
+  ControlLabel,
+  Radio,
+  Form,
+  Col,
+  Row,
 } from "react-bootstrap";
 import LoaderButton from "./LoaderButton";
 import "./Signup.css";
+import { SingleDatePicker } from "react-dates";
+import Select from 'react-select';
+import DatePicker from 'react-datepicker'
+
+
 
 export default class Signup extends Component {
   constructor(props) {
@@ -14,6 +23,8 @@ export default class Signup extends Component {
 
     this.state = {
       isLoading: false,
+      firstname: "",
+      lastname: "",
       email: "",
       password: "",
       confirmPassword: "",
@@ -30,11 +41,7 @@ export default class Signup extends Component {
     );
   }
 
-  validateConfirmationForm() {
-    return this.state.confirmationCode.length > 0;
-  }
-
-  handleChange = event => {
+   handleChange = event => {
     this.setState({
       [event.target.id]: event.target.value
     });
@@ -42,54 +49,64 @@ export default class Signup extends Component {
 
   handleSubmit = async event => {
     event.preventDefault();
+console.log("Here we aree!!");
+    
+      var username = this.state.username
+      var password = this.state.password
+      var firstname = this.state.firstname
+      var lastname = this.state.lastname
+      var gender = this.state.gender
 
-    this.setState({ isLoading: true });
+      var email = this.state.email
+      var dob = this.state.dob
 
-    this.setState({ newUser: "test" });
-
-    this.setState({ isLoading: false });
+      let userdata = JSON.stringify({
+          Username : username,
+          Password: password,
+          Firstname : firstname,
+          Lastname : lastname,
+          Gender : gender,
+          Email: email,
+          DateOfBirth: dob,
+          Family_ID : null
+        })
+        
+        fetch('http://localhost:46204/api/Users', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json', 
+              'Origin' :'http://localhost:46204', 
+              'Access-Control-Request-Method': 'POST',
+              'Access-Control-Request-Headers': 'Content-Type',
+              'Access-Control-Allow-Origin': '*'
+        },
+          body: userdata,
+        }).then(response => response.json()).then(json => console.log(json));
+    
+ 
   }
 
-  handleConfirmationSubmit = async event => {
-    event.preventDefault();
-
-    this.setState({ isLoading: true });
-  }
-
-  renderConfirmationForm() {
-    return (
-      <form onSubmit={this.handleConfirmationSubmit}>
-        <FormGroup controlId="confirmationCode" bsSize="large">
-          <ControlLabel>Confirmation Code</ControlLabel>
-          <FormControl
-            autoFocus
-            type="tel"
-            value={this.state.confirmationCode}
-            onChange={this.handleChange}
-          />
-          <HelpBlock>Please check your email for the code.</HelpBlock>
-        </FormGroup>
-        <LoaderButton
-          block
-          bsSize="large"
-          disabled={!this.validateConfirmationForm()}
-          type="submit"
-          isLoading={this.state.isLoading}
-          text="Verify"
-          loadingText="Verifying…"
-        />
-      </form>
-    );
-  }
 
   renderForm() {
     return (
         <form onSubmit={this.handleSubmit}>
+
+        <FormGroup controlId="username" bsSize="large">
+          <ControlLabel>Username</ControlLabel>
+          <FormControl
+            autoFocus
+            type="text"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+        </FormGroup>
+
+
         <FormGroup controlId="firstname" bsSize="large">
           <ControlLabel>FirstName</ControlLabel>
           <FormControl
             autoFocus
-            type="firstname"
+            type="text"
             value={this.state.firstname}
             onChange={this.handleChange}
           />
@@ -99,10 +116,20 @@ export default class Signup extends Component {
           <ControlLabel>LastName</ControlLabel>
           <FormControl
             autoFocus
-            type="lastname"
+            type="text"
             value={this.state.lastname}
             onChange={this.handleChange}
           />
+        </FormGroup>
+
+        <FormGroup controlId="gender" bsSize="large">
+          <ControlLabel>Gender</ControlLabel>
+              <FormControl
+            placeholder="Please enter M, F or O"
+            type="text"
+            value={this.state.gender}
+            onChange={this.handleChange}
+          /> 
         </FormGroup>
 
         <FormGroup controlId="email" bsSize="large">
@@ -113,6 +140,16 @@ export default class Signup extends Component {
             value={this.state.email}
             onChange={this.handleChange}
           />
+        </FormGroup>
+
+        <FormGroup controlId="dob" bsSize="large">
+          <ControlLabel>Date of Birth</ControlLabel>
+          <FormControl
+            value={this.state.dob}
+            onChange={this.handleChange}
+            type="date"  
+          />
+
         </FormGroup>
         <FormGroup controlId="password" bsSize="large">
           <ControlLabel>Password</ControlLabel>
@@ -130,15 +167,9 @@ export default class Signup extends Component {
             type="password"
           />
         </FormGroup>
-        <LoaderButton
-          block
-          bsSize="large"
-          disabled={!this.validateForm()}
-          type="submit"
-          isLoading={this.state.isLoading}
-          text="Signup"
-          loadingText="Signing up…"
-        />
+        <button block
+          bsSize="large" type="submit">Submit</button>
+
       </form>
     );
   }
@@ -146,9 +177,11 @@ export default class Signup extends Component {
   render() {
     return (
       <div className="Signup">
-        {this.state.newUser === null
-          ? this.renderForm()
-          : this.renderConfirmationForm()}
+        {
+          //this.state.newUser === null
+           this.renderForm()
+         // : this.renderConfirmationForm()
+        }
       </div>
     );
   }
